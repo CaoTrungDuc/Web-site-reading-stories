cb6ea28cfa0ead1bdd6abcc656f111cc54616e16
CaoTrungDuc <anhcomql123@gmail.com>
2021-03-13 12:01:33 +0700

    update

--- database/seeders/StoriesSeeder.php
+++ database/seeders/StoriesSeeder.php
@@ -39,6 +39,18 @@ class StoriesSeeder extends Seeder
                 'evaluate'=>5,
                 'created_at'=>now()
             ],
+            [
+                'id'=>3,
+                'name'=>'Phàm nhân tu tiên 3',
+                'avatar'=>'https://www.nae.vn/ttv/ttv/public/images/story/2de44ae0ec90c1a186b2fe0add591a9723db36c15f875b4a69966baf0c29b7b0.jpg',
+                'author'=>'Vong Ngữ',
+                'category'=>'Tiên hiệp',
+                'description'=>'Hay',
+                'status'=>2,
+                'comment'=>'Hay',
+                'evaluate'=>5,
+                'created_at'=>now()
+            ],
         ];
         Stories::insert($data);
     }
